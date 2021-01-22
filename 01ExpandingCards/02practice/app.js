b3b3126231bd58e703920ea4a3f71b0a9d95df48
Wissanu.khong <wissanu.khong@gmail.com>
2021-01-22 16:34:54 +0700

    feat: 02 practice

---
+++ 01ExpandingCards/02practice/app.js
@@ -0,0 +1,22 @@
+const pictures = document.querySelectorAll('.picture-container .picture');
+
+let i = 1;
+
+setInterval(() => {
+  // interation loop
+  i++;
+
+  // querySelector class
+  const picture = document.querySelector('.picture.active');
+
+  // removed classList
+  picture.classList.remove('active');
+
+  if (i > pictures.length) {
+    pictures[0].classList.add('active');
+    i = 1;
+  } else {
+    // nextElementSibbling
+    picture.nextElementSibling.classList.add('active');
+  }
+}, 1000);
